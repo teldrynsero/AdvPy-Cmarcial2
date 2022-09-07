b9fc79c7b7f5d18bd3263390e2282e20fb35cfb6
Chris Marcial <toastysnickers@gmail.com>
2022-09-07 15:33:32 -0600

    working

--- hangman/functions.py
+++ hangman/functions.py
@@ -1,13 +1,9 @@
 import random
 
+#for tracking player stats
 gamesPlayed = 0
 gamesWon = 0
 gamesLost = 0
-
-# HANGMAN FUNCTIONS
-# 3. record (record user's total games, wins, and loses)
-
-#images of the hangman
 
 # randomly chooses word
 def randomChoose():
@@ -22,14 +18,15 @@ def randomChoose():
         startGame(secretWord)
 
 #records player stats into stats.txt
-def stats(gamesPlayed,gamesLost):
+def stats(gamesPlayed,gamesLost,gamesWon):
     totalGamesPlayed = str(gamesPlayed)
     totalGamesLost = str(gamesLost)
+    totalGamesWon = str(gamesWon)
     f = open("stats.txt", "a")
-    f.write("Total games played: " + totalGamesPlayed + "\n" "Total games lost: " + totalGamesLost + "\n")
+    f.write("Total games played: " + totalGamesPlayed + "\n Total games lost: " + totalGamesLost + "\n Total games won: " + totalGamesWon)
     f.close()
 
-# start the actual game after getting user input
+#hangman game
 def startGame(secretWord):
     print("Game starting...")
     #hangman images
@@ -88,38 +85,63 @@ def startGame(secretWord):
     #hangman image counter
     x = 0
 
-    #while there is no game over
+    #list of letters in secret word
+    lst = []
+ 
+    for letter in secretWord:
+        lst.append(letter)
+ 
+    #see secret word for debugging
+    #print(lst)
+
+    #while player still has guesses left
     while strikes < 6:
         print(hangMan[x])
         guess = input("Enter a letter: ")
+        guess = guess.lower()
 
        #guess was a symbol or number; reject it and ask for input again
         if not guess.isalpha():
             print("Non-letter guesses are invalid: try again!")
         else:
             #guess is a letter
-            if guess.lower() in secretWord:
+            if guess in secretWord:
                 print("Correct!")
+                while guess in lst:
+                    lst.remove(guess)
+                    print(lst)
+
+                if len(lst) == 0:
+                    print("All letters guessed!")
+                    strikes = 7 #get out of loop
+                else:
+                    print("Still guesses left")
             else:
                 print("Wrong!")
                 strikes += 1
                 x += 1
 
-    #for letter in secretWord:
-    #    print()
+    global gamesPlayed
+    gamesPlayed += 1
+
+    #YOU WIN
+    if strikes == 7:
+        #increment player's wins
+        global gamesWon
+        gamesWon += 1
+
+        print("YOU WON! \n The secret word was " + secretWord)
 
     #GAME OVER you lost
     if strikes == 6:
         #print the completely hanged man, game over
         print(hangMan[6])
 
-        #increment player's games played and loses
-        global gamesPlayed
-        gamesPlayed += 1
+        #increment player's loses
         global gamesLost
         gamesLost += 1
 
-        print("GAME OVER! \n")
+        print("GAME OVER! \n The secret word was " + secretWord)
 
     answer = input("Want to play again? y/n \n")
     if answer.lower() == 'y':
@@ -129,7 +151,7 @@ def startGame(secretWord):
     else:
         print("Thank you for playing hangman! \n Returning to main menu...")
         #record player stats from game
-        stats(gamesPlayed,gamesLost)
+        stats(gamesPlayed,gamesLost,gamesWon)
 
 # show stat file
 def showStats():
